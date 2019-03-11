7c9643619d2ab7ef01916fabbc1083698986d0de
Andreas Peter <andreas.peter.ch@gmail.com>
2019-03-11 12:03:46 +0100

    Add tests for ising propagator

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using SimpleUpdate
 using Test
+using TensorOperations, TensorNetworkTensors, LinearAlgebra
 
 @testset "SimpleUpdate.jl" begin
-    # Write your own tests here.
+    @testset "Ising" begin
+        u = tfisingpropagator(0,1)
+        @test toarray(fuselegs(u,((1,2),(3,4)))[1]) ≈ I
+
+        u = tfisingpropagator(1e-3,1)
+        @tensor u2[1,2,3,4] := u[1,2,-1,-2] * u[-1,-2,3,4]
+        @test u2 ≈ tfisingpropagator(2e-3,1)
+        @test ishermitian(toarray(fuselegs(u,((1,2),(3,4)))[1]))
+    end
+
 end
